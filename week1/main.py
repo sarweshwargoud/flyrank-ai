84c08a8b862ce5cb69c9362d61d778d6568af5ef
Sarweshwar Buddolla <127119085+sarweshwargoud@users.noreply.github.com>
2026-07-15 17:34:50 +0530

    Stage 0: hello server

--- week1/main.py
+++ week1/main.py
@@ -2,42 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# In-memory data (our temporary database)
-tasks = [
-    {
-        "id": 1,
-        "title": "Learn FastAPI",
-        "done": False
-    },
-    {
-        "id": 2,
-        "title": "Learn CRUD",
-        "done": False
-    },
-    {
-        "id": 3,
-        "title": "Submit Assignment",
-        "done": True
-    }
-]
-
 @app.get("/")
-def home():
+def hello():
     return {
-        "name": "Task API",
-        "version": "1.0",
-        "endpoints": ["/tasks"]
+        "message": "Hello, World!"
     }
-
-@app.get("/health")
-def health():
-    return {
-        "status": "healthy"
-    }
-
-@app.get("/tasks/{task_id}")
-def get_tasks(task_id: int):
-    for task in tasks:
-        if task["id"] == task_id:
-            return task
-    return {"error": "Task not found"}
